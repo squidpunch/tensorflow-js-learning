bd8e74ab2746c656b7e23344add6eab48162756c
David Larrabee <dave@larrabeefamily.com>
2021-06-02 22:47:34 -0400

    Extracted face for additional processing

--- pages/cute-faces.tsx
+++ pages/cute-faces.tsx
@@ -2,35 +2,38 @@ import DemoPage from '../components/DemoPage';
 import * as tf from '@tensorflow/tfjs';
 import { useEffect } from 'react';
 
-const workIt = async (model: tf.LayersModel) => {
-  const petImage = document.getElementById('pet') as HTMLImageElement;
-
-  if (!petImage) return;
-
-  const myTensor = tf.browser.fromPixels(petImage);
+const getModelPrediction = (
+  model: tf.LayersModel,
+  imageTensor: tf.Tensor3D
+) => {
   // Model expects 256x256 0-1 value 3D tensor
-  const readyfied = tf.image
-    .resizeNearestNeighbor(myTensor, [256, 256], true)
+  const preppedModel = tf.image
+    .resizeNearestNeighbor(imageTensor, [256, 256], true)
     .div(255)
     .reshape([1, 256, 256, 3]);
 
-  const result = model.predict(readyfied) as tf.Tensor2D;
+  const result = model.predict(preppedModel) as tf.Tensor2D;
   // Model returns top left and bottom right
-  result.print();
+  // result.print();
+  const box = result.dataSync();
 
-  // Draw box on canvas
+  return box;
+};
+
+const annotateFace = async (
+  imgWidth: number,
+  imgHeight: number,
+  dimension: Float32Array | Int32Array | Uint8Array
+) => {
   const detection = document.getElementById('detection') as HTMLCanvasElement;
-  const imgWidth = petImage.width;
-  const imgHeight = petImage.height;
   detection.width = imgWidth;
   detection.height = imgHeight;
-  const box = result.dataSync();
-  const startX = box[0] * imgWidth;
-  const startY = box[1] * imgHeight;
-  const width = (box[2] - box[0]) * imgWidth;
-  const height = (box[3] - box[1]) * imgHeight;
-  const ctx = detection.getContext('2d');
+  const startX = dimension[0] * imgWidth;
+  const startY = dimension[1] * imgHeight;
+  const width = (dimension[2] - dimension[0]) * imgWidth;
+  const height = (dimension[3] - dimension[1]) * imgHeight;
 
+  const ctx = detection.getContext('2d');
   if (!ctx) return;
 
   ctx.strokeStyle = '#0F0';
@@ -38,16 +41,63 @@ const workIt = async (model: tf.LayersModel) => {
   ctx.strokeRect(startX, startY, width, height);
 };
 
+const prepareFaceTensor = async (
+  imageTensor: tf.Tensor3D,
+  dimension: Float32Array | Int32Array | Uint8Array
+) => {
+  const tensorHeight = imageTensor.shape[0];
+  const tensorWidth = imageTensor.shape[1];
+  const tensorStartX = dimension[0] * tensorWidth;
+  const tensorStartY = dimension[1] * tensorHeight;
+  const finalWidth = parseInt(
+    ((dimension[2] - dimension[0]) * tensorWidth) as unknown as string,
+    0
+  );
+  const finalHeight = parseInt(
+    ((dimension[3] - dimension[1]) * tensorHeight) as unknown as string,
+    0
+  );
+
+  const cropped = tf.slice(
+    imageTensor,
+    [tensorStartY, tensorStartX, 0],
+    [finalWidth, finalHeight, 3]
+  );
+
+  const faceTensor = tf.image
+    .resizeBilinear(cropped, [96, 96], true)
+    .reshape([1, 96, 96, 3]);
+
+  // faceTensor.print();
+
+  const croppedCanvas = document.getElementById('cropped') as HTMLCanvasElement;
+
+  await tf.browser.toPixels(cropped, croppedCanvas);
+};
+
 const detectFace = async () => {
   const modelPath = '/c5/model.json';
   const model = await tf.loadLayersModel(modelPath);
 
   tf.tidy(() => {
-    // shell out to another function in order to be able to run `await` :(
-    workIt(model);
-    model.dispose();
+    const petImage = document.getElementById('pet') as HTMLImageElement;
+
+    if (!petImage) return;
+
+    const imageTensor = tf.browser.fromPixels(petImage);
+    const imgWidth = petImage.width;
+    const imgHeight = petImage.height;
+    const box = getModelPrediction(model, imageTensor);
+
+    annotateFace(imgWidth, imgHeight, box);
+
+    prepareFaceTensor(imageTensor, box);
   });
-  console.log(tf.memory().numTensors);
+
+  model.dispose();
+
+  // verify memory cleanup
+  // console.log(tf.memory().numTensors);
 };
 
 const CuteFaces = () => {
@@ -64,6 +114,11 @@ const CuteFaces = () => {
         <img id="pet" src="/dog1.jpg" height="100%" />
         <canvas id="detection" className="left-0 absolute top-0"></canvas>
       </div>
+      <div className="font-bold">
+        We've pulled out the face created a tensor to send for more analysis,
+        the cropped image tensor data is respresented here:
+      </div>
+      <canvas id="cropped"></canvas>
     </DemoPage>
   );
 };
